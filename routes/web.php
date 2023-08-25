2094da82561266c9d315086223f908b17dd7056d
aspsptyd <devadiroot@gmail.com>
2023-08-25 23:37:06 +0700

    Set / route sebagai homepage

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/homepage', [HomepageController::class, 'index'])->name('homepage');
+Route::get('/', [HomepageController::class, 'index'])->name('homepage');
